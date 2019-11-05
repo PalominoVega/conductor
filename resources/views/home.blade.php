d87c8eb4c61731843601facb34ee698bc842b7a2
palominovega <paloominounprg@gmail.com>
2019-11-05 08:42:15 -0500

    menu aliniado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,42 @@
 @extends('layouts.admin')
 @section('content')
-
-<div class=home-flujo>
-    <h1>Bienvenido</h1>
     <div class="card my-3">
         <div class="card-header">
             <div class="card-title">Lista de Compras</div>
         </div>
+        <div class="table-responsive-general">
+            <table id="hola" class="table table-card table-striped">
+                <thead>
+                    <tr>
+                        <th>N°</th>
+                        <th>Item</th>
+                        <th>Item</th>
+                        <th>Opción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>Producto 01</td>
+                        <td>Producto 01</td>
+                        <td>
+                            <button class="btn-link-primary">
+                                <i class="fa fa-pencil"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>02</td>
+                        <td>Producto 02</td>
+                        <td>Producto 02</td>
+                        <td>
+                            <button class="btn-link-primary">
+                                <i class="fa fa-pencil"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,10 +30,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
   {{-- Booststrap  --}}
-  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"> 
-  {{-- datatable booststrap --}}
-  <link href="{{asset('css/dataTables.bootstrap4.css')}}" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   {{-- css app --}}
   <link href="{{asset('css/style.css')}}" rel="stylesheet">
@@ -126,14 +123,9 @@
     </footer> --}}
     
   {{-- booststrap y jquery --}}
-  <script src="{{asset('js/jquery.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script> 
- 
-  {{-- datateble--}}
-  <script src="{{asset('js/jquery.dataTables.js')}}"></script>
-  <script src="{{asset('js/dataTables.bootstrap4.js')}}"></script>
-  <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-  <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
   {{-- scripts app --}}
   {{-- alertas --}}
